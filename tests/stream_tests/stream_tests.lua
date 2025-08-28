32fdb6c1e40381279ac0ac05ee6a651eaff5a6df
remi.trotel <remi.trotel@epita.fr>
2025-08-28 18:36:17 +0200

    Added lazy evaluation and working iteration, and tests

--- tests/stream_tests/stream_tests.lua
+++ tests/stream_tests/stream_tests.lua
@@ -3,8 +3,9 @@ local Stream = require("tools.stream")
 
 local stream_tests = {}
 
+-- Test converting to a table
 function stream_tests.to_table()
-    local source = {
+    local input = {
         test1 = "test1",
         test2 = 2,
         test3 = -4,
@@ -12,12 +13,62 @@ function stream_tests.to_table()
         test5 = { test6 = 6 },
     }
 
-    local result = Stream.from(source):to_table()
+    local result = Stream.from(input):to_table()
 
-    assert(utils.count(source) == utils.count(result))
-    for key, value in pairs(source) do
+    assert(utils.count(input) == utils.count(result))
+    for key, value in pairs(input) do
         assert(result[key] == value)
     end
+end
+
+-- Test simple filtering
+function stream_tests.where_simple()
+    local input = { a = 1, b = 2, c = 3, d = 4 }
+    local result = Stream.from(input)
+        :where(function(_, v) return v % 2 == 0 end)
+        :to_table()
+
+    assert(result["b"] == 2)
+    assert(result["d"] == 4)
+    assert(result["a"] == nil)
+    assert(result["c"] == nil)
+end
+
+-- Test chaining multiple operations
+function stream_tests.where_chain()
+    local input = { a = 1, b = 2, c = 3, d = 4, e = 5 }
+    local result = Stream.from(input)
+        :where(function(_, v) return v > 1 end)
+        :where(function(_, v) return v % 2 == 0 end)
+        :to_table()
+
+    assert(result["b"] == 2)
+    assert(result["d"] == 4)
+    assert(result["a"] == nil)
+    assert(result["c"] == nil)
+    assert(result["e"] == nil)
+end
+
+-- Test empty table
+function stream_tests.empty_table()
+    local input = {}
+    local result = Stream.from(input):where(function(_, v) return true end):to_table()
+    assert(utils.count(result) == 0)
+end
+
+-- Test all filtered out
+function stream_tests.all_filtered()
+    local input = { x = 10, y = 20 }
+    local result = Stream.from(input):where(function(_, v) return v > 100 end):to_table()
+    assert(utils.count(result) == 0)
+end
+
+-- Test that keys and values are preserved
+function stream_tests.keys_and_values()
+    local input = { foo = "bar", hello = "world" }
+    local result = Stream.from(input):where(function(k, v) return k == "foo" end):to_table()
+    assert(result["foo"] == "bar")
+    assert(result["hello"] == nil)
 end
 
 return stream_tests
--- tools/stream.lua
+++ tools/stream.lua
@@ -4,27 +4,42 @@
 local Stream = {}
 Stream.__index = Stream
 
---- @generic TKey
---- @generic TValue
---- @param array table<TKey, TValue>
---- @return Stream<TKey, TValue>
-function Stream.from(array)
-    local stream = { _data = array }
+--- @param iterator_factory fun(table: table<TKey, TValue>, previous_key: TKey): TKey, TValue
+local function create_stream(iterator_factory)
+    local stream = { _iterator_factory = iterator_factory }
     setmetatable(stream, Stream)
     return stream
 end
 
-function Stream:iterate()
-    -- return self., self._data, nil
+--- @generic TKey
+--- @generic TValue
+--- @param array table<TKey, TValue>
+--- @return Stream<TKey, TValue>
+function Stream.from(table)
+    return create_stream(function()
+        return next, table, nil
+    end)
 end
 
+--- @generic TKey
+--- @generic TValue
+--- @param self Stream<TKey, TValue>
+--- @return fun(table: table<TKey, TValue>, previous_key: TKey): Tkey, TValue, table<TKey, TValue>, TKey
+function Stream:iterate()
+    -- Calls the factory to build the iterator, but still doesn't evaluate any values
+    return self._iterator_factory()
+end
+
+--- @generic TKey
+--- @generic TValue
+--- @param self Stream<TKey, TValue>
+--- @return table<TKey, TValue>
 function Stream:to_table()
-    -- local table = {}
-    -- for key, value in pairs(self:iterate()) do
-    --     table[key] = value
-    -- end
-    -- return table
-    return self._data
+    local table = {}
+    for key, value in self:iterate() do
+        table[key] = value
+    end
+    return table
 end
 
 --- @generic TKey
@@ -33,13 +48,23 @@ end
 --- @param predicate fun(key: TKey, value: TValue): boolean
 --- @return Stream<TKey, TValue>
 function Stream:where(predicate)
-    local result = {}
-    for key, value in pairs(self._data) do
-        if predicate(key, value) then
-            result[key] = value
+    function where_iterator_factory()
+        local iterator, table, previous_key = self._iterator_factory()
+        function where_iterator(table, previous_key)
+            while true do
+                local key, value = iterator(table, previous_key)
+                if key == nil then
+                    return nil
+                end
+                if predicate(key, value) then
+                    return key, value
+                end
+                previous_key = key
+            end
         end
+        return where_iterator, table, previous_key
     end
-    return Stream.from(result)
+    return create_stream(where_iterator_factory)
 end
 
 return Stream
